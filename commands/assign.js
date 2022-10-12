3d702c80304aa1e9cb9142bb9e901b548611322a
stryker-e <emily.stryker4@gmail.com>
2022-10-12 19:00:52 -0400

    commands base code

--- commands/assign.js
+++ commands/assign.js
@@ -1,10 +1,10 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('assign')
-        .setDescription('Assigns game interest roles to members.'),
-    async execute(interaction) {
-        await interaction.reply('Fuck off');
-    },
+	data: new SlashCommandBuilder()
+		.setName('assign')
+		.setDescription('Assigns game interest roles to members.'),
+	async execute(interaction) {
+		await interaction.reply('Fuck off');
+	},
 };
--- index.js
+++ index.js
@@ -3,29 +3,32 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Intents, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
-
+// const { Collection } = require('discord.js');
 // Create new client instance
-// const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.commands = new Collection();
+// client.commands = new Collection();
+// const commandsPath = path.join(__dirname, 'commands');
+// const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-// When client is ready, run code only once
-/*client.once('ready', () => {
-	console.log('Ready!');
-});
+// for (const file of commandFiles) {
+// 	const filePath = path.join(commandsPath, file);
+// 	const command = require(filePath);
+// 	client.commands.set(command.data.name, command);
+// }
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand()) return;
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-    const { commandName } = interaction;
-
-    if (commandName === 'ping') {
-        await interaction.reply('Pong!');
-    } else if (commandName === 'beep') {
-        await interaction.reply('Boop!');
-    }
-});*/
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args)); 
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 // Login to Discord with client's token
 client.login(token);
